6020ce790b44d6e64f90793ede85baa11ea7cb33
Mateusz Rzewnicki <m_rzewnicki@poczta.wwsi.edu.pl>
2020-10-19 07:14:42 +0200

    Add ReactApp

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt=>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt=>{
+                opt.AddPolicy("CorsPolicy", policy=>{
+                    policy.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -45,8 +50,9 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
