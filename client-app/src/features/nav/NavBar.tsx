067a78f689c49ce7f9e072eeaf6d22e285f251f0
Mateusz Rzewnicki <m_rzewnicki@poczta.wwsi.edu.pl>
2020-10-27 20:45:50 +0100

    End of fron section

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+interface IProps{
+    openCreateForm:()=>void;
+}
+
+const NavBar:React.FC<IProps> = ({openCreateForm}) => {
+    return (
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/> Reactivities
+                </Menu.Item>
+                <Menu.Item name='Activities' />
+                <Menu.Item>
+                    <Button positive content='Ceate Activity' onClick={openCreateForm}/>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+}
+
+export default NavBar;
